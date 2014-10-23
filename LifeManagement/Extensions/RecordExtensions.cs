d526be09fd94251e31097ba03b3b8ebc7c3addb7
Naya-san <akolesnik@bigmir.net>
2014-10-23 09:28:28 +0300

    Nice time format

---
+++ LifeManagement/Extensions/RecordExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LifeManagement.Models.DB;
+using LifeManagement.Resources;
+
+namespace LifeManagement.Extensions
+{
+    public static class RecordExtensions
+    {
+        public static string ConvertTimeToNice(this Record record)
+        {
+            if (!(record.EndDate.HasValue || record.StartDate.HasValue))
+            {
+                return ResourceScr.NoDueDate;
+            }
+            
+            if (record.EndDate.HasValue && record.StartDate.HasValue &&
+                record.StartDate.Value.Date == record.EndDate.Value.Date)
+            {
+                return String.Concat(ResourceScr.from, " ", record.StartDate.Value.ToString("t"), " ", ResourceScr.till, " ", record.EndDate.Value.ToString("t"), " ", record.EndDate.Value.ToString("yyyy MMMM dd") );
+            }
+            string res = "";
+            if (record.StartDate.HasValue)
+            {
+                res = String.Concat(ResourceScr.from, " ", record.StartDate.Value.ToString("g"), " ");
+            }
+
+            if (record.EndDate.HasValue)
+            {
+                res += String.Concat(ResourceScr.till, " ", record.EndDate.Value.ToString("g"));
+            }
+
+            return res;
+        }
+    }
+}
