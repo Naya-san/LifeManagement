c028565e187b7261e475db5fb4a234822e3ed61f
anna.kolesnyk <anna.kolesnyk@wonga.com>
2015-04-16 16:16:19 +0300

    Logic start

---
+++ LifeManagement/Logic/ListManager.cs
@@ -0,0 +1,16 @@
+﻿using LifeManagement.Models.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LifeManagement.Logic
+{
+    public static class ListManager
+    {
+        public static List<ListForDay> Generate (ApplicationDbContext db, TaskListSettingsViewModel settings){
+
+        }
+
+    }
+}
